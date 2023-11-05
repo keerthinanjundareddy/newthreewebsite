d5319932088f18249f68348db4327a40502cc1bd
keerthinanjundareddy <keerthana@apprikart.com>
2023-11-05 20:58:13 +0530

    me

--- src/pages/Layout1/Section.js
+++ src/pages/Layout1/Section.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState ,useEffect} from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
@@ -8,63 +8,100 @@ import "../../../node_modules/react-modal-video/scss/modal-video.scss";
 
 import HomeUrl from '../../assets/images/home-border.png';
 import Img from '../../assets/images/features/img-2.png';
+import axios from 'axios'
 
-class Section extends Component {
-  constructor () {
-    super()
-    this.state = {
-      isOpen: false
-    }
-    this.openModal = this.openModal.bind(this)
+const Section = () => {
+  const [homeapiData, sethomeApiData] = useState([])
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
   }
+
+//   const baseUrl = 'http://localhost:1337/api'
+//   useEffect(() => {
+//     axios.get('http://localhost:1337/api/website-homes?[populate][medialist][fields]=url&&[populate][medialist][fields]=alternativetext')
+//         .then((response) => {
+//             console.log("response",response.data.data[0])
+//             console.log("second responses",response.data.data[0].attributes.title)
+//             setAboutApiData(response.data.data)
+//             // setDescription(response.data.data[0].attributes.description)
+//             console.log("apidata",aboutapiData)
+//             // console.log("description here",description)
+//             // console.log("image",response.data.data[0].attributes.media.data[0].attributes)
+//         })
+// }, [])
  
-  openModal () {
-    this.setState({isOpen: true})
-  }
 
-  render() {
-    return (
-      <React.Fragment>
-        <section className="bg-home bg-light" id="home">
-          <div className="home-center">
-            <div className="home-desc-center">
-              <Container>
-                <Row className="align-items-center">
-                  <Col lg={6}>
-                    <div className="home-content">
-                      <p className="mb-0">Digital Marketing</p>
-                      <img src={HomeUrl} height="15" alt="" />
-                      <h1 className="home-title mt-4">Grow  <br />  </h1>
-                      <p className="text-muted mt-4 f-20">
-                                    quis tidunt vitae massa.</p>
-                      <div className="mt-4 pt-2">
-                        <Link to="#" className="btn btn-primary mr-3">Contact </Link>{" "}
-                        <Link to="#" className="video-play-icon text-dark" onClick={this.openModal}><i className="mdi mdi-play-circle-outline text-dark mr-2"></i>Watch Intro Video</Link>
-                      </div>
+const baseUrl = 'http://localhost:1337/api'
+useEffect(() => {
+  axios.get('http://localhost:1337/api/website-homes?[populate][medialist][fields]=url&&[populate][medialist][fields]=alternativetext')
+      .then((response) => {
+          console.log("response",response.data.data[0])
+          console.log("second responses",response.data.data[0].attributes.title)
+          sethomeApiData(response.data.data)
+          // setDescription(response.data.data[0].attributes.description)
+          console.log("apidata",homeapiData)
+          // console.log("description here",description)
+          // console.log("image",response.data.data[0].attributes.media_list.data[0].attributes)
+      })
+}, [])
+
+    // const imageUrl = baseUrl + (item.attributes.media_list?.data?.[0]?.attributes?.url || '');
+  return (
+    <React.Fragment>
+
+      <section className="bg-home bg-light" id="home">
+      {
+  homeapiData.length > 0  && homeapiData.map((itemstwo)=>{
+    const imageUrltwo = baseUrl + (itemstwo.attributes.medialist?.data?.[0]?.attributes.url || '');
+    return(
+      <div  key={itemstwo.id}>
+        <div className="home-center">
+          <div className="home-desc-center">
+            <Container>
+              <Row className="align-items-center">
+                <Col lg={6}>
+                  <div className="home-content">
+                    <p className="mb-0">{itemstwo.attributes.heading}</p>
+                    <img src={HomeUrl} height="15" alt="" />
+                    <h1 className="home-title mt-4">{itemstwo.attributes.heading} <br />{itemstwo.attributes.heading}   </h1>
+                    <p className="text-muted mt-4 f-20">
+                    {itemstwo.attributes.description}
+                    </p>
+                    <div className="mt-4 pt-2">
+                      <Link to="#" className="btn btn-primary mr-3"> {itemstwo.attributes.contactbutton}</Link>{" "}
+                      <Link to="#" className="video-play-icon text-dark" onClick={openModal}>
+                        <i className="mdi mdi-play-circle-outline text-dark mr-2"></i>Watch Intro Video
+                      </Link>
                     </div>
-                  </Col>
-                  <Col lg={6}>
-                    <div className="home-img">
-                      <div className="animation-1"></div>
-                      <div className="animation-2"></div>
-                      <div className="animation-3"></div>
-                      <img src={Img} className="img-fluid" alt="" />
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </div>
-            <ModalVideo
-              channel="vimeo"
-              isOpen={this.state.isOpen}
-              videoId="99025203"
-              onClose={() => this.setState({ isOpen: false })}
-            />
+                  </div>
+                </Col>
+                <Col lg={6}>
+                  <div className="home-img">
+                    <div className="animation-1"></div>
+                    <div className ="animation-2"></div>
+                    <div className="animation-3"></div>
+                    <img src={imageUrltwo}className="img-fluid" alt="" />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
           </div>
-        </section>
-      </React.Fragment>
-    );
-  }
+          <ModalVideo
+            channel="vimeo"
+            isOpen={isOpen}
+            videoId="99025203"
+            onClose={() => setIsOpen(false)}
+          />
+        </div>
+        </div>
+          )
+    })
+    }
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default Section;
