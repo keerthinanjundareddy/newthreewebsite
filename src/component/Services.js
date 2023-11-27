0f0f6fc4a7b5ab658628b93f9eaa440f35243465
keerthinanjundareddy <keerthana@apprikart.com>
2023-11-27 11:26:57 +0530

    image section css added

--- src/component/Services.js
+++ src/component/Services.js
@@ -36,10 +36,10 @@ const Services = () => {
                   <Col lg={4} key={card.id} style={{display:"flex",flexWrap:"wrap"}}>
                     
                     <div className="services-box p-4 mt-4" style={{ minHeight: "20px",minWidth: "100px", width: "100%" }}>
-                      <div className="services-icon bg-soft-primary">
+                      <div className="services-icons bg-soft-primary" style={{width:"55px",height:"55px",textAlign:"center"}}>
                     
                       {card.Icon && 
-                      <div style={{ width: "100%", height: "100%"}}>
+                      <div style={{ width: "100%", height: "100%",textAlign:"center"}}>
                       <img src={`${baseUrl}${card.Icon.url}`} alt={card.AlternativeTextOfIcon} style={{ width:"100%",height:"100%",objectFit:"contain",padding:"10px" }} />
                       </div>
                       }
