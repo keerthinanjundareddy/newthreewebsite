1dffbe5abbc8aa1219ed591651b662bf257c4f75
keerthinanjundareddy <keerthana@apprikart.com>
2023-11-29 09:27:17 +0530

    message

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -15,9 +15,9 @@ export default class Contact extends Component {
               <Col lg={12}>
                 <div className="title-box text-center">
                   <h3 className="title-heading mt-4">Let's talk about everything!</h3>
-                  <p className="text-muted f-17 mt-3">Vivamus ac nulla ultrices laoreet neque mollis mi morbi
+                  {/* <p className="text-muted f-17 mt-3">Vivamus ac nulla ultrices laoreet neque mollis mi morbi
                   elementum mauris
-                            sit amet arcu <br /> fringilla auctor In eleifend maximus nisi sed vulputate.</p>
+                            sit amet arcu <br /> fringilla auctor In eleifend maximus nisi sed vulputate.</p> */}
                   <img src={HomeUrl} height="15" className="mt-3" alt="" />
                 </div>
               </Col>
--- src/component/Services.js
+++ src/component/Services.js
@@ -98,11 +98,11 @@ const Services = () => {
             <div className="services-box p-4 mt-4" style={{ minHeight: "20px", minWidth: "100px", width: "100%" }}>
             
                 {card.images && (
-                    <div className="services-icons  bg-soft-primary" style={{ width: "100%", height: "200px" }}>
-                  <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
+                    // <div className="services-icons  bg-soft-primary" style={{ width: "100%", height: "200px" }}>
+                  <div style={{ width: "100%", height:"200px", textAlign: "center" }}>
                  
                     <img src={`${baseUrl}${card.images.url}`} alt={card.images.filename} style={{ width: "100%", height: "100%", objectFit: "contain", padding: "10px" }} />
-                  </div>
+                  {/* </div> */}
                   </div>
                 )}
              
