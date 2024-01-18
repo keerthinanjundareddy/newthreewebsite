eaa58ac8675202b45a69b15bc8efcc1d2930aa15
keerthinanjundareddy <keerthana@apprikart.com>
2024-01-18 11:28:39 +0530

    added FAQ section

--- src/component/Readmore.js
+++ src/component/Readmore.js
@@ -4,8 +4,20 @@ import imageone from '../assets/images/users/img-1.jpg'
 import logo from '../assets/Userimages/shopifyimage.png'
 import Footer from './Footer/Footer'
 import isibisilogo from '../assets/Userimages/isibisi-removebg-preview.png'
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+
 
 function Readmore() {
+    const accordionData = [
+        { title: "Accordion Demo 1", content: "Greetings of the day 1" },
+        { title: "Accordion Demo 2", content: "Greetings of the day 2" },
+        // Add more objects as needed
+      ];
+    
   return (
     <>
    <div className='header-section'>
@@ -40,7 +52,25 @@ function Readmore() {
 
         </div>
       
-
+        <div style={{marginTop:"20px"}}>
+      <h4 style={{textAlign:"center"}}>FAQ</h4>
+      <div style={{marginTop:"10px"}}>
+        {accordionData.map((item, index) => (
+          <Accordion key={index} style={{ width: "100%" ,marginTop:"10px",border:"none",outline:"none"}}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel${index + 1}-content`}
+              style={{ backgroundColor: "#D3D3D3",border:"1px solid #D3D3D3" }}
+            >
+              <Typography style={{ fontWeight: 10 }}>{item.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails style={{ backgroundColor: "#D3D3D3",border:"1px solid #D3D3D3" }}>
+              <Typography>{item.content}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </div>
       
     </div>
     <div style={{padding:"0px"}}>
