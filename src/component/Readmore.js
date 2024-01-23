b997a11aac11eb2e76bd3edc6d5d21a61c19505d
keerthinanjundareddy <keerthana@apprikart.com>
2024-01-23 12:29:20 +0530

    added features section ui

--- src/component/Readmore.js
+++ src/component/Readmore.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Readmore.css'
 import imageone from '../assets/images/users/img-1.jpg'
 import logo from '../assets/Userimages/imagetwo.png'
@@ -13,6 +13,16 @@ import leftArrow from '../assets/Userimages/leftarow.png'
 import shopifyimaaage from '../assets/Userimages/Is-it-worth-it-to-hire-a-Shopify-expert.jpg'
 import shopifyimage from '../assets/Userimages/Mask group.png'
 import shopifylogo from '../assets/Userimages/Shopify-Emblem.png'
+
+
+// import imageonee from '../../src/assets/Userdummyimages/Useo-search-engine-optimization-2023-05-08-23-44-00-utc.jpg'
+import imagetwo from '../../src/assets/Userdummyimages/social-media-2022-11-14-07-05-26-utc.jpg'
+import imagethree from '../../src/assets/Userdummyimages/image-of-two-friendly-business-partner-working-on-2023-04-18-02-34-32-utc.JPG'
+import imagefour from '../../src/assets/Userdummyimages/social-media-2022-11-14-07-05-26-utc.jpg'
+import imagefive from '../../src/assets/Userdummyimages/assessment-analysis-bar-graph-presentation-2022-12-16-00-26-45-utc - Copy.jpg'
+import imagesix from '../../src/assets/Userdummyimages/image-of-start-up-business-people-discussing-over-2023-01-31-03-28-56-utc.JPG'
+import imageseven from '../../src/assets/Userdummyimages/a-man-chats-with-an-artificial-intelligence-chat-b-2023-08-29-05-17-51-utc.jpg'
+
 
 
 function Readmore() {
@@ -36,11 +46,22 @@ function Readmore() {
         },
         // Add more objects as needed
       ];
+
+      const dummyData = [
+        { title: imageone, heading: 'SEARCH ENGINE OPTIMIZATION (SEO)', contenttwo: "SEO, or Search Engine Optimization, is all about keywords, enhancing online presence, and on-page optimization to boost brand awareness and organic traffic. Continuous monitoring is vital for optimizing results." },
+        { title: imagetwo, heading: 'PAY-PER-CLICK ADVERTISING (PPC)', contenttwo: "PPC Advertising, commonly associated with Google AdWords, is an online marketing campaign that directs clicks to a specific landing page. It aims to create impressions, generate clicks, and maximize Return on Investment (ROI)." },
+        { title: imagethree, heading: 'SOCIAL MEDIA MARKETING (SMM)', content: 'Louis Phillippe', contenttwo: "SMM, or Social Media Marketing, involves building brand loyalty through strategic content creation, community management, and a strong social media presence. It often leverages influencer partnerships and engaging campaigns to connect with the target audience. enhance branding and engage the audience effectively" },
+        { title: imagefour, heading: 'CONTENT MARKETING', contenttwo: "Content marketing is a strategy that focuses on creating and distributing valuable content such as blog posts, articles, and infographics. It involves optimization and tracking metrics to enhance branding and engage the audience effectively." },
+        { title: imagefive, heading: 'CONVERSION RATE OPTIMIZATION (CRO)', content: 'Coco-Cola', contenttwo: "Conversion Rate Optimization (CRO) is all about optimizing websites for higher engagement and conversions. It involves techniques like A/B testing, improving user experience, and enhancing the conversion funnel, often through effective call-to-action strategies." },
+        { title: imagesix, heading: 'REMARKETING AND RETARGETING', content: 'Amway', contenttwo: "Remarking and retargeting in online advertising involve personalized ads and tracking to optimize conversations and encourage conversions. It's a strategic approach within online advertising to re-engage potential customers who have shown previous interest." },
+        { title: imageseven, heading: 'EMERGING TRENDS AND TECHNOLOGIES', content: 'Ummeed Finance', contenttwo: "Emerging trends and technologies encompass Artificial Intelligence, chatbots, voice search, and automation. These innovations are reshaping the digital landscape, influencing user experiences, and driving the evolution of various industries." },
+      ];
       
+      const [hoveredCard, setHoveredCard] = useState(null);  
     
   return (
     <>
-   <div className='header-section'>
+   <div className='header-section' style={{zIndex:"100"}}>
     <div style={{width:"100px",height:"40px"}}><img src={isibisilogo} style={{width:"100%",height:"100%",objectFit:"contain"}} /></div>
     <div className='back-btn'><span><img src={leftArrow} style={{width:"20px",height:"20px",objectFit:"contain",paddingRight:"4px"}} /></span>Home</div>
    </div>
@@ -100,42 +121,29 @@ function Readmore() {
 
 
       <div className='shopify-features-section'>
-        <div className='shopify-flexbox-container'>
-        <div className='shopify-feaatures-content-section' >
-          <h1 style={{textAlign:"left"}}>Heading</h1>
-          <div>Migrating data from one system to another system can be 
-cumbersome especially in Shopify. Since Shopify works based 
-on templates, their Product template is limited in terms of 
-properties. They do offer metafields as a solution but that 
-is not even sufficient to migrate the old data structure.
-Migrating data from one system to another system can be 
-cumbersome especially in Shopify. Since Shopify works based 
-on templates, their Product template is limited in terms of 
-properties. They do offer metafields as a solution but that 
-is not even sufficient to migrate the old data structure.</div>
-
-        </div>
-        <div className='shopify-fetures-image-section'>
-      <div className="circle-container">
-<ul className="feature-list" >
-    <li className='featureone'>Feature 1 features 2 features 3 </li>
-    <li className='featuretwo'>Feature 2 features 2</li>
-    <li className='feturethree'>Feature 3 features 2</li>
-  
-  </ul>
-  <div className="circle-image">
-    <img src={shopifylogo} alt="shopifylogo" />
-  </div>
-  <ul className="feature-list">
-    <li className='featurefour'>Feature 4</li>
-    <li className='featurefive'>Feature 5</li>
-    <li className='featuresix'>Feature 6</li>
+      <div style={{textAlign:"center",color:"green",marginTop:"20px",fontSize:"20px"}}><b>OUR-OFFERS(DIGITAL AND PERFORMANCE MARKETING)</b></div>
+    <div className="card-container">
+       {dummyData.map((data, index) => (
+       <div
+       key={index}
+       className="card"
    
-  </ul>
-</div>
-</div>
+       onMouseEnter={() => setHoveredCard(index)}
+       onMouseLeave={() => setHoveredCard(null)}
+     >
+          <div  className='imge-sections'>
+             <img src={data.title}  alt="imgone" className='imge-section-two' />
+            </div>
+       <div className='heading-sections'style={{textTransform:"uppercase",marginTop:"40px",textAlign:"center",}}>{data.heading}</div>
+          <div className='title-sections'  style={{ opacity: hoveredCard === index ? 1: 1}}>{data.contenttwo}</div>
 
-      </div>
+ 
+          
+  </div>
+      ))}
+    
+   
+</div>
       </div>
       
         <div className='faq-top-section'>
